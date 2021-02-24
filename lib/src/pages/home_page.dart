818e667d81d443b3b121917c91eef7bd543d1500
Darwin Uzcategui Portati Linux <darwin.uzcategui1973@gmail.com>
2021-02-24 06:12:01 -0400

    titulo deprecado

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,15 +13,12 @@ import 'package:barcode_scan/barcode_scan.dart';
 
 import 'package:pruebamapa/src/utils/utils.dart' as utils;
 
-
 class HomePage extends StatefulWidget {
-  
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   final scansBloc = new ScansBloc();
 
   int currentIndex = 0;
@@ -33,7 +30,7 @@ class _HomePageState extends State<HomePage> {
         title: Text('QR Scanner'),
         actions: <Widget>[
           IconButton(
-            icon: Icon( Icons.delete_forever ),
+            icon: Icon(Icons.delete_forever),
             onPressed: scansBloc.borrarScanTODOS,
           )
         ],
@@ -42,83 +39,72 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: _crearBottomNavigationBar(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        child: Icon( Icons.filter_center_focus ),
-        onPressed: ()=> _scanQR( context ),
+        child: Icon(Icons.filter_center_focus),
+        onPressed: () => _scanQR(context),
         backgroundColor: Theme.of(context).primaryColor,
       ),
     );
   }
 
   _scanQR(BuildContext context) async {
-
     // https://fernando-herrera.com
     // geo:40.724233047051705,-74.00731459101564
     // geo:10.595929,-67.0436865
 
     // String futureString = '';
     // dynamic futureString='geo:10.595929,-67.0436865';
-    dynamic futureString ;
+    dynamic futureString;
 
     try {
-     
-       futureString = await BarcodeScanner.scan();
-    } catch(e) {
+      futureString = await BarcodeScanner.scan();
+    } catch (e) {
       futureString = e.toString();
     }
 
-    if ( futureString != null ) {
-      
-      final scan = ScanModel( valor: futureString.rawContent );
-      scansBloc.agregarScan(scan);      
+    if (futureString != null) {
+      final scan = ScanModel(valor: futureString.rawContent);
+      scansBloc.agregarScan(scan);
 
-      if ( Platform.isIOS ) {
-        Future.delayed( Duration( milliseconds: 750 ), () {
-          utils.abrirScan(context, scan);    
+      if (Platform.isIOS) {
+        Future.delayed(Duration(milliseconds: 750), () {
+          utils.abrirScan(context, scan);
         });
       } else {
         utils.abrirScan(context, scan);
       }
-
     }
-     
-
   }
 
-
-  Widget _callPage( int paginaActual ) {
-
-    switch( paginaActual ) {
-
-      case 0: return MapasPage();
-      case 1: return DireccionesPage();
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
 
       default:
         return MapasPage();
     }
-
   }
 
   Widget _crearBottomNavigationBar() {
-
     return BottomNavigationBar(
       currentIndex: currentIndex,
       onTap: (index) {
         setState(() {
-          currentIndex = index; 
+          currentIndex = index;
         });
       },
       items: [
         BottomNavigationBarItem(
-          icon: Icon( Icons.map ),
-          title: Text('Mapas')
-        ),
+            icon: Icon(Icons.map),
+            // ignore: deprecated_member_use
+            title: Text('Mapas')),
         BottomNavigationBarItem(
-          icon: Icon( Icons.brightness_5 ),
-          title: Text('Direcciones')
-        ),
+            icon: Icon(Icons.brightness_5),
+            // ignore: deprecated_member_use
+            title: Text('Direcciones')),
       ],
     );
-
-
   }
 }
